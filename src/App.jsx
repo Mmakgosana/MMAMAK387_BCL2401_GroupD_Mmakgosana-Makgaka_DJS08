ce5915d575d130f9fb4bf629fe25d6ded3e32fc6
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-04 13:50:28 +0200

    I have created server.js file

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+import "./server";
+
 
 export default function App() {
     return (
         <BrowserRouter>
           <header>
-            <Link to="/">#VANLIFE</Link>
+            <Link className="site-logo" to="/">#VANLIFE</Link>
             <nav>
                 <Link to="/about"></Link>
             </nav>
