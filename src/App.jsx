2d0ba8ad745bb71f7b8d5cbcae840500ec250299
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-04 14:31:16 +0200

    I have loaded the van elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
 
-import "./server";
+import "./server"
 
-
-export default function App() {
-    return (
-        <BrowserRouter>
-          <header>
-            <Link className="site-logo" to="/">#VANLIFE</Link>
-            <nav>
-                <Link to="/about">About</Link>
-            </nav>
-          </header>
-            <Routes>
-               <Route path="/" element={<Home />}/>
-               <Route path="/about" element={<About />}/> 
-            </Routes>
-        
-        </BrowserRouter>
-    )
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />)
-
+  .render(<App />);
