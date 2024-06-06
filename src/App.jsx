eeb0d8863bd1a9f7175f5c8802491ebd6e213a99
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-06 11:21:47 +0200

    I have fixed the Navbar with a Layout route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 import "./server"
 
@@ -13,10 +14,13 @@ export default function App() {
     <BrowserRouter>
       
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
+        
       </Routes>
     </BrowserRouter>
   )
