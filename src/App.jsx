49150e21a3c30fc0627521e0f02ffdb39cfac73e
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-07 12:55:24 +0200

    I have added the vans page in the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
+ 
 
 
 import "./server"
@@ -19,8 +21,8 @@ export default function App() {
     <BrowserRouter>
       
       <Routes>
-        <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+        <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="vans" element={<Vans />} />
             <Route path="/vans/:id" element={<VanDetail />} />
@@ -28,9 +30,10 @@ export default function App() {
 
             
             <Route path="/host" element={<HostLayout />}>
-              <Route index element={<Dashboard />} />
+              <Route  element={<Dashboard />} />
               <Route path="income" element={<Income />} />
               <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
             </Route>
 
 
