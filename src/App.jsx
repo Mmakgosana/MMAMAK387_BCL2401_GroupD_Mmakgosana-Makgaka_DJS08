9d5cc1087215ab31d362b62926c180d2e81b3d6b
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-10 11:23:21 +0200

    I have added the hostVanInfo and HostVanPricing to the host pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,37 +10,39 @@ import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import HostVans from "./pages/Host/HostVans"
 import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
- 
-
 
 import "./server"
 
 export default function App() {
+  
+  
   return (
     <BrowserRouter>
-      
       <Routes>
-        <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<VanDetail />} />
-
-
-            
-            <Route path="/host" element={<HostLayout />}>
-              <Route  element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path="vans" element={<HostVans />} />
-              <Route path=":id" element={<HostVanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+              
             </Route>
-
-
+          </Route>
         </Route>
-        
       </Routes>
     </BrowserRouter>
   )
